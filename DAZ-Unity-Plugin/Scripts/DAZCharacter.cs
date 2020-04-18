f5a1511b8e9b0b0c8e3d48cb614441b99dac52e9
Michael Martinsen <migo@tv2.dk>
2020-04-18 21:55:37 +0200

    fixed line endings

--- DAZ-Unity-Plugin/Scripts/DAZCharacter.cs
+++ DAZ-Unity-Plugin/Scripts/DAZCharacter.cs
@@ -2,12 +2,21 @@
 using UnityEngine;
 
 [AddComponentMenu("Cintix/DAZ 3D/Character")]
-public class DAZCharacter : MonoBehaviour {    public delegate void MorphChange(string name, float value);    public event MorphChange OnMorphChange;    public Dictionary<string, Transform[]> orginalBones = new Dictionary<string, Transform[]>();
+public class DAZCharacter : MonoBehaviour {
+    public delegate void MorphChange(string name, float value);
+    public event MorphChange OnMorphChange;
+
+    public Dictionary<string, Transform[]> orginalBones = new Dictionary<string, Transform[]>();
     public SkinnedMeshRenderer[] ignornedMeshRenderers;
     public SkinnedMeshRenderer sourceRenderer;
 
     public int sourceRenderIndex;
-    public bool expandIgnore = false;    public bool expandMorphs = false;    // Start is called before the first frame update    void Start() {
+    public bool expandIgnore = false;
+    public bool expandMorphs = false;
+
+
+    // Start is called before the first frame update
+    void Start() {
         
     }
 
@@ -16,7 +25,15 @@ public class DAZCharacter : MonoBehaviour {    public delegate void MorphChang
 
     }
 
-
-    public void setMorphValue(int index, float value) {        if (sourceRenderer != null) {            string name = sourceRenderer.sharedMesh.GetBlendShapeName(index);            if (name.StartsWith(sourceRenderer.name.Substring(0, sourceRenderer.name.IndexOf(".Shape")))) {                name = name.Substring(name.IndexOf("__"));            }            sourceRenderer.SetBlendShapeWeight(index, value);            if (OnMorphChange != null) OnMorphChange(name, value);        }    }
+    public void setMorphValue(int index, float value) {
+        if (sourceRenderer != null) {
+            string name = sourceRenderer.sharedMesh.GetBlendShapeName(index);
+            if (name.StartsWith(sourceRenderer.name.Substring(0, sourceRenderer.name.IndexOf(".Shape")))) {
+                name = name.Substring(name.IndexOf("__"));
+            }
+            sourceRenderer.SetBlendShapeWeight(index, value);
+            if (OnMorphChange != null) OnMorphChange(name, value);
+        }
+    }
 
 }
--- DAZ-Unity-Plugin/Scripts/MasterMorph.cs
+++ DAZ-Unity-Plugin/Scripts/MasterMorph.cs
@@ -9,16 +9,54 @@ public class MasterMorph : MonoBehaviour {
 
     public DAZCharacter character;
 
-    public void setDazCharacter(DAZCharacter daz) {        character = daz;        _sourceRenderer = GetComponent<SkinnedMeshRenderer>();        setupMorphes();    }
+    public void setDazCharacter(DAZCharacter daz) {
+        character = daz;
+        _sourceRenderer = GetComponent<SkinnedMeshRenderer>();
+        setupMorphes();
+    }
 
-    private void OnEnable() {        setupMorphes();    }
+    private void OnEnable() {
+        setupMorphes();
+    }
 
-    private void Awake() {        setupMorphes();    }    private void Start() {        setupMorphes();    }
+    private void Awake() {
+        setupMorphes();
+    }
 
-    private void setupMorphes() {        _sourceRenderer = GetComponent<SkinnedMeshRenderer>();        if (_sourceRenderer != null && character != null) {            int blendShapeCounts = _sourceRenderer.sharedMesh.blendShapeCount;            for (int index = 0; index < blendShapeCounts; index++) {                string name = _sourceRenderer.sharedMesh.GetBlendShapeName(index);                if (!_morphIndexes.ContainsKey(name)) {                    _morphIndexes.Add(name, index);                }            }            character.OnMorphChange += OnMorphChanged;        }    }
+    private void Start() {
+        setupMorphes();
+    }
 
-    public void unregister() {        character.OnMorphChange -= OnMorphChanged;        if (Application.isEditor) DestroyImmediate(this);        else Destroy(this,0);    }
+    private void setupMorphes() {
+        _sourceRenderer = GetComponent<SkinnedMeshRenderer>();
+        if (_sourceRenderer != null && character != null) {
+            int blendShapeCounts = _sourceRenderer.sharedMesh.blendShapeCount;
+            for (int index = 0; index < blendShapeCounts; index++) {
+                string name = _sourceRenderer.sharedMesh.GetBlendShapeName(index);
+                if (!_morphIndexes.ContainsKey(name)) {
+                    _morphIndexes.Add(name, index);
+                }
+            }
+            character.OnMorphChange += OnMorphChanged;
+        }
+    }
 
-    private void OnMorphChanged(string name, float value) {        if (name.StartsWith("__")) {            string newName = _sourceRenderer.name.Substring(0, _sourceRenderer.name.IndexOf(".Shape")) + name;            name = newName;        }        if (_morphIndexes.ContainsKey(name)) {            int index = _morphIndexes[name];            _sourceRenderer.SetBlendShapeWeight(index, value);        }    }
+    public void unregister() {
+        character.OnMorphChange -= OnMorphChanged;
+        if (Application.isEditor) DestroyImmediate(this);
+        else Destroy(this,0);
+    }
+
+    private void OnMorphChanged(string name, float value) {
+        if (name.StartsWith("__")) {
+            string newName = _sourceRenderer.name.Substring(0, _sourceRenderer.name.IndexOf(".Shape")) + name;
+            name = newName;
+        }
+
+        if (_morphIndexes.ContainsKey(name)) {
+            int index = _morphIndexes[name];
+            _sourceRenderer.SetBlendShapeWeight(index, value);
+        }
+    }
 
 }
